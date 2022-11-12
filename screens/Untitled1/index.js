4b80368d20b8cbc80138a6f4f763db2303513dc5
Firstsvcadghgdy Lastxpcnbluxjr <automation3@crowdbotics.com>
2022-11-12 02:42:44 +0000

    Crowdbotics Studio: Update app screens

---
+++ screens/Untitled1/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+
+const Untitled1 = () => {
+  return <View style={_styles.cAPbCBMu}>
+      </View>;
+};
+
+export default Untitled1;
+
+const _styles = StyleSheet.create({
+  cAPbCBMu: {
+    backgroundColor: "#f0f0f1",
+    padding: 10,
+    position: "relative",
+    height: "100%"
+  }
+});
